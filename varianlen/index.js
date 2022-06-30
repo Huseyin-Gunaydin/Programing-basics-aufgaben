f6eb713927ff350cdfc3fcec07253d12b1a2e89f
= <=>
2022-06-30 12:42:04 +0200

    variablen gestellt

---
+++ varianlen/index.js
@@ -0,0 +1,27 @@
+/*
+ * Variablen
+ *
+ * Was sind Variablen?
+ * Variablen sind wie Boxen denen man einen Namen geben kann
+ * Und in denen man Datentypen ablegen kann
+ *
+ *
+ */
+
+/**
+ * Die erste Art Variablen zu deklarieren ist mir "var"
+ * var, dann der Name der Variable, dann ein Gleichzeichen,
+ * und dann der Wert der darin abgelegt werden soll
+ */
+var nameDerVariable = "Banane";
+
+/**
+ * Mit let kann man Boxen deklarieren in die man Dinge rein und wieder raus nehmen kann
+ */
+let auchEinVariablenName = "Apfel";
+
+/**
+ * Mit const deklariert man "Konstanten". Das sind Boxen in die man nur ein mal etwas rein legen kann
+ * Danach kann man es nicht mehr verändern
+ */
+const blablaEinWeitererName = "Birne";
---
+++ varianlen/variablen-zusammenfuegen.js
@@ -0,0 +1,30 @@
+/**
+ * Variablen zusammenfügen und verändern
+ */
+
+const essen1 = "Pizza";
+const essen2 = "Burger";
+const essen3 = "Döner";
+const essen4 = "Pfannkuchen";
+const essen5 = "Pommes";
+
+const marvinsLieblingsessen = essen3;
+console.log("Marvins Lieblingsessen:", marvinsLieblingsessen);
+
+const marcelsLieblingsessen = essen1;
+console.log("Marcels Lieblingsessen:", marcelsLieblingsessen);
+
+const yaaroubsLieblingsessen = essen5 + " " + essen3;
+console.log("Yaaroubs Lieblingsessen:", yaaroubsLieblingsessen);
+
+/**
+ * Wie sollte man Variablen bennenen?
+ * Der erste Buchstabe ist immer klein
+ * Alle weiteren Wörter beginnen mit großbuchstaben
+ * z.B anzahlTüren
+ *
+ * Handelt es sich um eine super wichtige bombenfeste nie veränderbare * Konstante, also const Variable schreibt man den namen GANZ_GROSS
+ */
+
+const SERVER_PASSWORT = "qweuohasdhq876127836";
+const API_PATH = "api.example.com/api/v2";
