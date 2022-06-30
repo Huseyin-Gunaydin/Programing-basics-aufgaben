88d286aed94e061b546ab690cd9241de453d0abc
= <=>
2022-06-30 12:38:23 +0200

    comments

---
+++ introduction/nodejs/index.js
@@ -0,0 +1,19 @@
+/*
+ * JavaScript Dateien kann man direkt über NodeJS ausführen mit:
+ * node name-der-datei.js
+ *
+ * Oder man führt sie mit nodemon aus.
+ * Vorteil: Man muss nicht händisch immer wieder das Script ausführen
+ *
+ *
+ *
+ * Installiere nodemon mit:
+ * sudo npm install -g nodemon
+ * oder
+ * npm install -g nodemon
+ *
+ * Führe JavaScript Dateien aus mit:
+ * nodemon name-der-datei.js
+ */
+
+console.log("Hello World ABCDFFFFsadas");
