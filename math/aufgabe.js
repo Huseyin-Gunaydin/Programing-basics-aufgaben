2e37b8ffea959d2ca3f96cbb564cbad9e303fb36
= <=>
2022-07-06 21:18:19 +0200

    mart aufgabe fertig gemacht

---
+++ math/aufgabe.js
@@ -0,0 +1,29 @@
+// const randomNummer= Math.floor((Math.random()*10)+);
+//   if(randomNummer > 5){
+//     console.log("Ja");
+//   }else{
+//     console.log("nein");
+//   }
+
+
+
+//   const randomNummer1 = Math.floor(Math.random() * 6 + 1);
+//   console.log(randomNummer1);
+//   if (randomNummer1 == 6) {
+//   console.log("Noch einmal");
+// } else {
+//   console.log("Tschuss");
+// }
+
+
+for (var randomNummer= Math.floor(Math.random()*6+1); randomNummer++;){
+    console.log(randomNummer);
+      if (randomNummer !== 6) {
+        console.log("Zahl nicht 6");
+         break;
+     }else{
+      console.log("Super! Nummer6")
+     }
+    }
+
+    
