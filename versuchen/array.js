045996f8fe72fa113bb8c85de42809b1ac4fd495
= <=>
2022-07-06 10:35:00 +0200

    arrays datai erstellt

--- versuchen/array.js
+++ versuchen/array.js
@@ -6,39 +6,42 @@ let handys = ["iphone 12", "iphone 13", "iphone 14"];
 let preise = ["800", "900", "1100"];
 let farbe = ["silber", "schwarz", "weiß"];
 
+console.log(preise[3]);
 
 
-let produkt1 =["iphone 12", 800, "silber"];
-let produkt2 =[];
-let produkt3 =["iphone 14",
- 1100,
-  ["weiß", "schwarz", "blau"]];
+// let produkt1 =["iphone 12", 800, "silber"];
+// let produkt2 =[];
+// let produkt3 =["iphone 14",
+//  1100,
+//   ["weiß", "schwarz", "blau"]];
 
 
- produkt2[0] = "iphone 13";
- produkt2[1] = 900; 
- produkt2[2] = "schwarz"; 
-
-//  produkt3[2] = "blau";
-
- console.log(produkt3[2][0]);
- console.log(produkt3[2][1]);
- console.log(produkt3[2][2]);
 
 
-// console.log(produkt1);
-// console.log(produkt2);
-// console.log(produkt3);
+//  produkt2[0] = "iphone 13";
+//  produkt2[1] = 900; 
+//  produkt2[2] = "schwarz"; 
 
-// console.log(preise[0]);
-// console.log(preise[1]);
-// console.log(preise[2]);
+// //  produkt3[2] = "blau";
 
-// console.log(`${handys[0]} - ${preise[0]} - ${farbe[0]}`);
-// console.log(`${handys[1]} - ${preise[1]} - ${farbe[1]}`);
-// console.log(`${handys[2]} - ${preise[2]} - ${farbe[2]}`);
+//  console.log(produkt3[2][0]);
+//  console.log(produkt3[2][1]);
+//  console.log(produkt3[2][2]);
 
 
-let kursAdi = "Komple Web Gelistirme Egitimi"
-console.log(kursAdi[5]);
-console.log(kursAdi.split(" ")[1]);
+// // console.log(produkt1);
+// // console.log(produkt2);
+// // console.log(produkt3);
+
+// // console.log(preise[0]);
+// // console.log(preise[1]);
+// // console.log(preise[2]);
+
+// // console.log(`${handys[0]} - ${preise[0]} - ${farbe[0]}`);
+// // console.log(`${handys[1]} - ${preise[1]} - ${farbe[1]}`);
+// // console.log(`${handys[2]} - ${preise[2]} - ${farbe[2]}`);
+
+
+// let kursAdi = "Komple Web Gelistirme Egitimi"
+// console.log(kursAdi[5]);
+// console.log(kursAdi.split(" ")[1]);
