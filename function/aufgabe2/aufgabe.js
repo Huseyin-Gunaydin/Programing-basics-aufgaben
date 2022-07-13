37e8e9b288d506f99f209c3b104b624a4aed97b8
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-07-13 21:38:13 +0200

    function datei erstellt

---
+++ function/aufgabe2/aufgabe.js
@@ -0,0 +1,29 @@
+
+
+
+// function istGeradeZahl(zahl){
+//     const Boolean = Math.abs(zahl % 2 === 0);
+//     return Boolean;
+// }
+
+// console.log(istGeradeZahl(3),"soll false sein");
+// console.log(istGeradeZahl(10),"soll true sein");
+// console.log(istGeradeZahl(7),"soll false sein");
+// console.log(istGeradeZahl(40),"soll true sein");
+
+
+
+// function istGeradeZahl(zahl){
+//     if (Math.abs(zahl%2===0)){
+//        return true;
+//     }
+//     else{
+//         return false;
+//     }
+//  }
+//  console.log(istGeradeZahl(3),"soll false sein");
+//  console.log(istGeradeZahl(10),"soll true sein");
+//  console.log(istGeradeZahl(7),"soll false sein");
+//  console.log(istGeradeZahl(40),"soll true sein");
+
+
--- function/index.js
+++ function/index.js
@@ -1,25 +1,34 @@
-/****
+/**
  * Funktionen
- * 
+ *
  * Was sind Funktionen?
- * 
+ *
  * Funktionen sind Code-Abschnitte die man aufrufen kann wenn man sie ausführen möchte.
- * 
- *Man verhindert also dass man den selben Code mehrmals schreiben muss.
+ * Man verhindert also dass man den selben Code mehrmals schreiben muss.
+ *
+ *
+ * Wie sehen Funktionen aus?
+ */
 
- Wie sehen Funktionen aus?
- * 
- * 
- * ***/
+console.log("1");
 
- function carp(a,b){
-    console.log(a+b);
- }
- carp(6,2);
+// Funktionsaufruf
+nameDerFunktion();
 
+// Funktionsdeklaration
+function nameDerFunktion() {
+  // Der Code zwischen den geschweiften Klammern ist in der Funktion
+  console.log("In der Funktion123");
+}
 
+console.log("2");
 
- function vergihesapla(fiyat){
-    const kdv = fiyat *0.19;
- }
- carp(6,2);
+// Funktionsaufruf
+nameDerFunktion();
+
+console.log("3");
+
+console.log("4");
+
+// Funktionsaufruf
+nameDerFunktion();
