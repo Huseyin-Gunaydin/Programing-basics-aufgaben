95b489f04028a139695c87e8ec8428f26bf48cf0
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-07-17 10:44:04 +0200

    aufgabe fertig gemacht

--- scopes/index.js
+++ scopes/index.js
@@ -1,32 +1,38 @@
-var name1 = "Hüseyin";  // Global
+var name1 = "Hüseyin"; // Global
 const stadt = "Berlin";
 
-function text(){
-    var name1 = "Vural";
-    var age = 30;
-    console.log("function scope: ", name1, age);
+function text() {
+  var name1 = "Vural";
+  var age = 30;
+  console.log("function scope: ", name1, age);
+}
+console.log(name1);
+text();
+
+
+
+// console.log(age);   nicht global
+
+
+
+if (true) {
+  let name1 = "Rahman";
+  let nachname = "Günaydin";
+  // var name1 ="Rahman";
+  console.log(name1);
+  const stadt = "Istanbul";
+  console.log(stadt);
 }
 
+console.log(stadt)
 
-// console.log(name1);
-// // console.log(age);   nicht global
+// console.log(name1, stadt);
 
 
 
-if(true){
-    let name1 ="Rahman";
-    let nachname="Günaydin"
-    // var name1 ="Rahman";
-    console.log(name1);
-    const stadt = "Istanbul";
-    console.log(stadt)
-}
-console.log(name1, stadt);
 
 // console.log(nachname);     nachname if icinde tanimlandigi icin disaridan cagiramayiz.
 
-
-text();
 
 
 //Fonktionlar kendi scopes alanlarini olustururlar. Block icinde yeni bir scope olusmaz.
