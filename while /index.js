9fb0e004c89daa65717b93690b49c66dee4d6fd4
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-07-18 22:58:29 +0200

    aufgabe fertig gemacht

--- while /index.js
+++ while /index.js
@@ -1,18 +1,19 @@
 let a= 0;
+let b="Hüseyin"
 
 while(a<10){
-    console.log(a);
+    console.log(a+"."+b);
     a++
 }
 
 
-var toplam=0;
-var say=1;
-var sayi;
+// var toplam=0;
+// var say=1;
+// var sayi;
 
-while(sayi!=0){
-    sayi=prompt(say+".sayi giriniz");
-    toplam=toplam +Number(sayi);
-    say++
-}
-console.log(say+"sayinin toplami"+toplam);
+// while(sayi!=0){
+//     sayi=prompt(say+".sayi giriniz");
+//     toplam=toplam +Number(sayi);
+//     say++
+// }
+// console.log(say+"sayinin toplami"+toplam);
