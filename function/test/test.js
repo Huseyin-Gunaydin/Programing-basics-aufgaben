94e5ac3576f86e7c3181ab0808d24ede7fa6b6ef
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-07-25 21:18:26 +0200

    geläst

--- function/test/test.js
+++ function/test/test.js
@@ -64,13 +64,3 @@ function vowelCount(text) {
 
 // DO NOT EDIT below this line - This will result in an automatic fail
 // ÄNDERE NICHTS unterhalb dieser Zeile - Sonst schlägt der automatische Test fehl
-module.exports = {
-    nameOfCity,
-    isDivisible,
-    missingAngle,
-    isRaining,
-    geometricalSequence,
-    multiplesOfThree,
-    powerOf,
-    vowelCount,
-};
