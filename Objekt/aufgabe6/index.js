7766942b93e55dcdb7987f5b9a6fddfd5e070e04
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-08-11 12:26:02 +0200

    aufgabe6 gelöst

---
+++ Objekt/aufgabe6/index.js
@@ -0,0 +1,11 @@
+function isWithinRange (num){
+
+
+}
+
+
+
+isWithinRange(4, { min: 0, maks: 5 }) 
+isWithinRange(4, { min: 4, maks: 5 }) 
+isWithinRange(4, { min: 6, maks: 10 }) 
+isWithinRange(5, { min: 5, maks: 5 }) 
