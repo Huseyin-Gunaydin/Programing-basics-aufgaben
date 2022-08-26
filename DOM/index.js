ed9f7502487e07cf9b4bbc3419554bf88e85601c
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-08-26 23:52:23 +0200

    Every gelernt

--- DOM/index.js
+++ DOM/index.js
@@ -64,7 +64,7 @@ const urunler = [
 // //dizide yazdigimiz kosulu saglayan eleman bulunuyorsa true döndürüyor ya da false döndürüyor.
 //console.log("Some", urunler.some((urun) => urun.fiyat <= 2));
 
-// //dizide yazdigimiz kosulu bütün elemanlar sagliyor mu saglamiyor mu diye bakar. Eger sagliyorsa true döndürür. saglamiyorsa false döndürür.
+izide yazdigimiz kosulu bütün elemanlar sagliyor mu saglamiyor mu diye bakar. Eger sagliyorsa true döndürür. saglamiyorsa false döndürür.
 
 console.log(
   "Every: ",
