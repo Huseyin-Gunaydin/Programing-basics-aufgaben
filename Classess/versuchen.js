b6d2947077333b27e31f54ec477775ec7e039858
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-08-29 21:57:36 +0200

    aufgabe fertig

--- Classess/versuchen.js
+++ Classess/versuchen.js
@@ -1,3 +1,6 @@
+
+
+
 class person {
   constructor(name, nachname, geburtsjahr) {
 
@@ -11,32 +14,25 @@ class person {
   }
 }
 
-class ogrenci extends person {
+
+
+class student extends person {
   constructor(name, nachname, geburtsjahr, beruf) {
    super(name, nachname, geburtsjahr);
     this.beruf = beruf;
   }
 }
 
+
+
+
+
 const huseyinGunaydin = new person("Hüseyin", "Günaydin", 1985);
-let sahis = new ogrenci("Hüseyin", "Günaydin", 1985, "Webentwickler");
+let sahis = new student("Hüseyin", "Günaydin", 1985, "Webentwickler");
 
 console.log(huseyinGunaydin);
 console.log(huseyinGunaydin.wiealt())
-
 console.log(sahis);
 console.log(sahis.wiealt())
 
 
-function findUppercase(str) {
-  if(str.length === 0) return "Es gibt keine große Buchstaben."
-
-  if(str.charAt(0) === str.charAt(0).toUpperCase()) {
-      return str.charAt(0)
-  } else {
-      return findUppercase(str.substring(1));
-  }
-  
-}
-
-console.log(findUppercase("asHZdkjfalajdjfald"));
