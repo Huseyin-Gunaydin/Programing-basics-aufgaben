3a30af90e38da3773357f446e417bd2632b49c1b
Huseyin-Gunaydin <gunaydinhuseyin1453@gmail.com>
2022-08-30 22:05:45 +0200

    Die nächsten Schritte

--- Classess/Digitale Restaurant/index.js
+++ Classess/Digitale Restaurant/index.js
@@ -6,17 +6,124 @@
 
 
 
-
-const Speisekarte = [{menu:1}]
-
-
-const Kasse = 150;//Euro
-
-
-class Restaurant{
-    constructor(){
+// Restaurant
+// + Speisekarte
+// + geöffnet/geschlossen
+// + Kasse
+// - Personal bezahlen
 
 
 
+class Restaurant {
+    constructor (speisekarte, kassenbestand) {
+        this.speisekarte = speisekarte;
+        this.kasse = kassenbestand;
+        this.geoeffnet = false;
+    }
+
+    personalBezahlen() {
+        // ...
+    }
+
+    oeffnen() {
+        this.geoeffnet = true;
+    }
+
+    schliessen() {
+        this.geoeffnet = false;
+    }
+
+    // DEBUG --------
+    status() {
+        console.log("geöffnet?", this.geoeffnet);
     }
 }
+
+const restaurant = new Restaurant([], 0);
+
+
+// Personal
+// + Anfangswert ausgezahlter Lohn: 0€
+// - Lohn erhalten
+class Personal {
+    constructor() {
+        this.lohn = 0;
+    }
+
+    lohnErhalten(lohn) {
+        this.lohn += lohn;
+    }
+}
+
+const personal = new Personal();
+
+// Kellner (erbt von Personal)
+// - Bestellung aufnehmen
+// - Bestellung weitergeben
+// - Bestellung ausgeben
+// - abkassieren
+class Kellner extends Personal {
+    bestellungAufnehmen(gericht) {
+        console.log("Bestellung aufgenommen:", gericht);
+        // ...
+    }
+
+    bestellungWeitergeben() {
+        // ...
+    }
+
+    bestellungAusgeben() {
+        // ...
+    }
+
+    abkassieren(gericht) {
+        console.log("Bestellung abgerechnet:", gericht);
+        // ...
+    }
+}
+
+// Koch (erbt von Personal)
+// - Bestellung erhalten
+// - Bestellung zubereiten
+
+
+// Gast
+// - bestellen
+// - bezahlen
+class Gast {
+    constructor(kellner) {
+        this.kellner = kellner;
+    }
+
+    bestellen(gericht) {
+        this.gericht = gericht;
+        this.kellner.bestellungAufnehmen(gericht);
+    }
+
+    bezahlen() {
+        console.log(`zu bezahlen: ${this.gericht.preis}€`);
+        this.kellner.abkassieren(this.gericht);
+    }
+}
+
+
+
+const kellner = new Kellner();
+
+const gast1 = new Gast(kellner);
+const gast2 = new Gast(kellner);
+
+gast1.bestellen({ name: "Pizza", preis: 10 });
+gast2.bestellen({ name: "Pasta", preis: 8 });
+
+gast1.bezahlen();
+gast2.bezahlen();
+
+
+
+// Die nächsten Schritte:
+// Klasse Bestellung erstellen
+// Alle wichtigen Infos in der Bestellung speichern (Gericht, Preis, Gast,...)
+// Bestellungen im Restaurant speichern, damit alle Zugriff darauf haben
+// An alle Personen die Restaurant-Instanz übergeben, damit sie sich "alle im gleichen Restaurant befinden"
+// Kellner erzeugen neue Bestellungen und fügen Sie beim Restaurant hinzu
