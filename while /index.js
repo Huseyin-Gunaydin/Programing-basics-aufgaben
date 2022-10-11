dcef18af296738f6d15497723309a6bd9c259376
Huseyin-Gunaydin <gunaydinhsueyin1453@gmail.com>
2022-10-11 20:49:34 +0200

    deneme

--- while /index.js
+++ while /index.js
@@ -7,13 +7,13 @@ while(a<10){
 }
 
 
-// var toplam=0;
-// var say=1;
-// var sayi;
+var toplam=0;
+var say=1;
+var sayi;
 
-// while(sayi!=0){
-//     sayi=prompt(say+".sayi giriniz");
-//     toplam=toplam +Number(sayi);
-//     say++
-// }
-// console.log(say+"sayinin toplami"+toplam);
+while(sayi!=0){
+    sayi=prompt(say+".sayi giriniz");
+    toplam=toplam +Number(sayi);
+    say++
+}
+console.log(say+"sayinin toplami"+toplam);
